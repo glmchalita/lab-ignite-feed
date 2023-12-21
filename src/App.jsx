32f8d37e7a54d51d3002afb0ada744c407caf710
Guilherme Chalita <guilhermechalita@hotmail.com>
2023-12-21 20:05:23 -0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+import "./global.css"
+
+export default function App() {
+
+  return (
+    <>
+      <Header />
+      <h1>Hello World</h1>
+    </>
+  )
+}
