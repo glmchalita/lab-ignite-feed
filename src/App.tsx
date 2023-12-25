8452c446130ab82e990aeaaef8a7a10749e112e9
Guilherme Chalita <guilhermechalita@hotmail.com>
2023-12-25 12:35:29 -0300

    change JavaScript to TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-import Post from "./components/Post";
+import Post, { PostType } from "./components/Post";
 
 import styles from './App.module.css'
 
 import "./global.css"
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -49,9 +49,7 @@ export default function App() {
             return (
               <Post 
                 key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
+                post={post}
               />
             )
           })}
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -4,35 +4,59 @@ import ptBr from 'date-fns/locale/pt-BR'
 import Avatar from './Avatar'
 import Comment from './Comment'
 import styles from './Post.module.css'
-import { useState } from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
 
-export default function Post({author, publishedAt, content}) {
-  const [comments, setComments] = useState([])
+interface Author {
+  name: string
+  role: string
+  avatarUrl: string
+}
+
+interface Content {
+  type: 'paragraph' | 'link'
+  content: string
+}
+
+export interface PostType {
+  id: number
+  author: Author
+  content: Content[]
+  publishedAt: Date
+}
+
+interface PostProps {
+  post: PostType
+}
+
+export default function Post({ post }: PostProps) {
+  const [comments, setComments] = useState([
+    'Dhr'
+  ])
   
   const [newCommentText, setNewCommentText] = useState('')
 
-  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+  const publishedDateFormatted = format(post.publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBr,
   })
 
-  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+  const publishedDateRelativeToNow = formatDistanceToNow(post.publishedAt, {
     locale: ptBr,
     addSuffix: true,
   })
   
   const isNewCommentEmpty = newCommentText.length === 0
 
-  function handleCreateNewComment() {
+  function handleCreateNewComment(event: FormEvent) {
     event.preventDefault()
     setComments([...comments, newCommentText])
     setNewCommentText('')
   }
 
-  function handleNewComment() {
+  function handleNewComment(event: ChangeEvent<HTMLTextAreaElement>) {
     setNewCommentText(event.target.value)
   }
 
-  function deleteComment(commentToDelete) {
+  function deleteComment(commentToDelete: string) {
     const commentsWithoutDeletedOne = comments.filter(comment => {
       return comment !== commentToDelete
     })
@@ -44,23 +68,23 @@ export default function Post({author, publishedAt, content}) {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src={author.avatarUrl} />
+          <Avatar src={post.author.avatarUrl} />
           
           <div className={styles.authorInfo}>
-            <strong>{author.name}</strong>
-            <span>{author.role}</span>
+            <strong>{post.author.name}</strong>
+            <span>{post.author.role}</span>
           </div>
         </div>
 
-        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
+        <time title={publishedDateFormatted} dateTime={post.publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
       </header>
 
       <div className={styles.content}>
-        {content.map(line => {
+        {post.content.map(line => {
           if (line.type === 'paragraph') {
-            return <p key={line.contet}>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === 'link') {
-            return <p key={line.contet}><a href='#'>{line.content}</a></p>
+            return <p key={line.content}><a href='#'>{line.content}</a></p>
           }
         })}
       </div>
